66d56c8bc24b705f23be46d75f577d80cdf7a10a
Blackmonkcr <aaron.navarro@utec.edu.pe>
2023-11-16 11:47:41 -0500

    Struct modify - Testing

--- HashTable_Cuckoo.h
+++ HashTable_Cuckoo.h
@@ -14,7 +14,6 @@ class HashTable_Cuckoo {
     size_t size{};
     int numberReHashes{};
     const float maxFill_Factor = 0.80;
-    const int MAX_COLLISION = 5;
     Pair<TK, TV>* primary_table = nullptr;
     Pair<TK, TV>* secondary_table = nullptr;
 
@@ -71,7 +70,7 @@ size_t getNewCapacityCuckoo(size_t capacity) {
 
 template<typename TK, typename TV>
 void HashTable_Cuckoo<TK, TV>::insertHelper(TK key, TV value, int typeHashing, int cont) {
-    if (size >= capacity * maxFill_Factor || cont >= size) {
+    if (size >= (capacity * 2) * maxFill_Factor || cont >= size) {
         rehash(getNewCapacityCuckoo(capacity));
     }
     if (typeHashing == 0) {
